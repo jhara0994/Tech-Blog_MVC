36d7ba84594b2ab77126af76c0c64248e82c00a1
Jared Haralson <jharalson29@gmail.com>
2022-02-08 17:58:10 -0500

    Updating models and adding a JSON seed file for testing

--- models/index.js
+++ models/index.js
@@ -4,10 +4,11 @@ const Comment = require('./Comment')
 
 Blog.hasMany(Comment, {
   foreignKey: 'blogData_id',
+  onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
+Comment.belongsTo(Blog, {
+  foreignKey: 'blogData_id'
 })
 
 
--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,34 @@
 const sequelize = require('../config/connection');
-const blogSeed = require('./blogData');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogSeed = require('./blogData.json');
 const commentSeed = require('./commentData');
+const posts = []
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await blogSeed();
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  })
 
-  await commentSeed();
+  for (const post of blogSeed) {
+    post.push(await Blog.create ({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    }))
+  }
+
+  for (const comment of commentSeed) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blog_id: posts[Math.floor(Math.random() * users.length)].id,
+    })
+  }
+
 
   process.exit(0);
 };
