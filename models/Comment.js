ae80cac4212555df60d72cb66e6dbfb8e0f5caf2
Jared Haralson <jharalson29@gmail.com>
2022-02-07 23:05:20 -0500

    Updating the model and seeds for comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,3 +2,43 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
+
+Comment.init(
+    {
+        user: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'blog_id'
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+)
+
+module.exports = Comment;
--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,1 +1,32 @@
 const { Comment } = require('../models');
+
+const commentData = [
+  {
+    user: 'LadyUx',
+    user_id: 3,
+    post_date: 'February 10, 2022',
+    blog_id: 2,
+    description:
+      'Learning the difference between authorization and authentication is crucial to understand to be a quality developer.',
+  },
+  {
+    user: 'SirTechie',
+    user_id: 2,
+    post_date: 'December 05, 2021',
+    blog_id: 3,
+    description: 
+        'Handlebars is one of the most functional tools I have found and love having it in me repertoire.',
+  },
+  {
+    user: 'JavaMan',
+    user_id: 1,
+    post_date: 'January 10, 2022',
+    blog_id: 1,
+    description: 
+        'MVC has helped me greatly in my career, and I suggest all developers learn this framework.',
+  },
+];
+
+const commentSeeds = () => Comment.bulkCreate(commentData);
+
+module.exports = commentSeeds;
