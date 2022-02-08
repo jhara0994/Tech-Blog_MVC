eaa896607c80352d0ff0d42f7d8acec230a56ca9
Jared Haralson <jharalson29@gmail.com>
2022-02-07 22:38:11 -0500

    Pushing seeds and model for blog data

--- models/Blog.js
+++ models/Blog.js
@@ -2,3 +2,45 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Blog extends Model {}
+
+Blog.init(
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+    }
+)
+
+module.exports = Blog;
