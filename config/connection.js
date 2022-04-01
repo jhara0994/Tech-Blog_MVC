6b1f9548eb248f7d8de148466e65e86383f02abd
Jared Haralson <jharalson29@gmail.com>
2022-04-01 17:42:56 -0400

    Retrying connection files to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     });
+let sequelize;
 
-// module.exports = sequelize;
-
-let sequelize
-
-if(process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL)
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'heroku_2149f8eb2c3beaa',
+    'b5ce8f2bfce663',
+    '8a7d7405',
     {
-      host: 'localhost',
+      host: 'us-cdbr-east-05.cleardb.net',
       dialect: 'mysql',
       port: 3306
     }
-  )
+  );
 }
 
-module.exports = sequelize
-
+module.exports = sequelize;
