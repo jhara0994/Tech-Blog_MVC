4d35b7e2734b6499fc5824c58aa929c6ac9be9b6
Jared Haralson <jharalson29@gmail.com>
2022-04-01 17:40:46 -0400

    Changes connections for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,32 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+// module.exports = sequelize;
+
+let sequelize
+
+if(process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   sequelize = new Sequelize(
-    'heroku_2149f8eb2c3beaa',
-    'b5ce8f2bfce663',
-    '8a7d7405',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: 'us-cdbr-east-05.cleardb.net',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
-  );
+  )
 }
 
-module.exports = sequelize;
+module.exports = sequelize
+
